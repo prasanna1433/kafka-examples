cd12acef149ed3a312e6a6b4a0b691d6997114cd
Prasanna Subburaj <prasanna.subburaj@gmail.com>
2018-04-21 16:55:22 -0500

    Initial Processor API example commit

---
+++ src/main/java/io/github/prasanna1433/streams/IntermediateProcess.java
@@ -0,0 +1,27 @@
+package io.github.prasanna1433.streams;
+
+import org.apache.kafka.streams.processor.Processor;
+import org.apache.kafka.streams.processor.ProcessorContext;
+
+public class IntermediateProcess implements Processor<String,String> {
+
+    private ProcessorContext context;
+
+    public void init(ProcessorContext context) {
+        this.context = context;
+    }
+
+    public void process(String key, String value) {
+        context.forward(key,value);
+        context.commit();
+    }
+
+    public void punctuate(long timestamp) {
+
+    }
+
+    public void close() {
+
+    }
+
+}
---
+++ src/main/java/io/github/prasanna1433/streams/ProcessorAPI.java
@@ -0,0 +1,42 @@
+package io.github.prasanna1433.streams;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+
+import java.util.Properties;
+
+public class ProcessorAPI {
+    public static void main(String[] args){
+
+        if(args.length<2){
+            System.out.println("Please send the input arguments <boostrapServer> <applicationId>");
+        }
+
+        String bootstrapServer = args[0];
+        String applicationId= args[1];
+
+        Properties streamsConfiguration = new Properties();
+        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG,applicationId);
+        streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+        Topology builder = new Topology();
+
+        // add the source processor node that takes Kafka topic "source-topic" as input
+        builder.addSource("Source", "source-topic")
+
+                // add the WordCountProcessor node which takes the source processor as its upstream processor
+                .addProcessor("Process", () -> new IntermediateProcess(), "Source")
+
+                // add the sink processor node that takes Kafka topic "sink-topic" as output
+                // and the WordCountProcessor node as its upstream processor
+                .addSink("Sink", "sink-topic", "Process");
+
+        KafkaStreams kafkaStreams=new KafkaStreams(builder,streamsConfiguration);
+        kafkaStreams.start();
+
+    }
+}
